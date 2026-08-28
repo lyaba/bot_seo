7ed53adc95d063b9b1fbcc818127fc38f54b08ac
lyaba <albertka54243@gmail.com>
2026-08-28 19:09:32 +0300

    Update haibo proxy config loading

--- bot-haibo/proxies.js
+++ bot-haibo/proxies.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '.env'), quiet: true });
 
 const DEFAULT_HAIBO_PROXY_HOST = '209.101.201.73';
 const DEFAULT_HAIBO_PROXY_PORT = '59100';
